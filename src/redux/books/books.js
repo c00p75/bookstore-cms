752a6d2b41a58f446f193ea92ec032f77c61ab8a
c00p75_ <georgecoopmsapenda@gmail.com>
2023-01-31 15:01:28 +0200

    Use duck pattern action types. Add REMOVE_BOOK switch case

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,6 @@
 // Initializing constatnt action variables
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const defaultState = [];
 
@@ -8,6 +8,10 @@ export const booksReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.book];
+
+    case REMOVE_BOOK:
+      return [...state.filter((book) => book.id !== action.id)];
+
     default:
       return state;
   }
